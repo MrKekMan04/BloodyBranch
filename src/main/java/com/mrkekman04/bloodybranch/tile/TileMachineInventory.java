cd54ecf37b74325997ee6cc2cfe7fb1b0349adfa
Vitaliy <87753891+MrKekMan04@users.noreply.github.com>
2021-11-14 13:05:05 +0500

    create base tile

---
+++ src/main/java/com/mrkekman04/bloodybranch/tile/TileMachineInventory.java
@@ -0,0 +1,176 @@
+package com.mrkekman04.bloodybranch.tile;
+
+import com.mrkekman04.bloodybranch.reference.Reference;
+import com.mrkekman04.bloodybranch.utils.interfaces.ICustomInventory;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.ItemStackHelper;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.NonNullList;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.world.World;
+
+public class TileMachineInventory extends TileEntity implements ICustomInventory {
+
+    protected NonNullList<ItemStack> inventory;
+
+    public TileMachineInventory(int slots) {
+        super();
+        inventory = NonNullList.<ItemStack>withSize(slots, ItemStack.EMPTY);
+    }
+
+    @Override
+    public ITextComponent getDisplayName() {
+        return new TextComponentTranslation(getName());
+    }
+
+    @Override
+    public String getName() {
+        return "container." + Reference.MOD_ID + ".";
+    }
+
+    @Override
+    public boolean hasCustomName() {
+        return false;
+    }
+
+    @Override
+    public int getSizeInventory() {
+        return inventory.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        for (ItemStack itemstack : inventory)
+        {
+            if (!itemstack.isEmpty())
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean shouldRefresh(World world, BlockPos pos, IBlockState oldState, IBlockState newState) {
+        return (oldState.getBlock() != newState.getBlock());
+    }
+
+    @Override
+    public ItemStack getStackInSlot(int index) {
+        return inventory.get(index);
+    }
+
+    @Override
+    public ItemStack decrStackSize(int index, int count) {
+        return ItemStackHelper.getAndSplit(inventory, index, count);
+    }
+
+    @Override
+    public ItemStack removeStackFromSlot(int index) {
+        return ItemStackHelper.getAndRemove(inventory, index);
+    }
+
+    @Override
+    public void setInventorySlotContents(int index, ItemStack stack) {
+        inventory.set(index, stack);
+
+        if (stack.getCount() > this.getInventoryStackLimit())
+        {
+            stack.setCount(this.getInventoryStackLimit());
+        }
+    }
+
+    @Override
+    public int getInventoryStackLimit() {
+        return 64;
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound compound)
+    {
+        super.readFromNBT(compound);
+        inventory = NonNullList.<ItemStack>withSize(this.getSizeInventory(), ItemStack.EMPTY);
+        ItemStackHelper.loadAllItems(compound, inventory);
+    }
+
+    @Override
+    public NBTTagCompound writeToNBT(NBTTagCompound compound)
+    {
+        super.writeToNBT(compound);
+        ItemStackHelper.saveAllItems(compound, inventory);
+
+        return compound;
+    }
+
+    public int getComparatorOutput() {
+        //Same math as Container.calcRedstoneFromInventory
+        int count = 0;
+        float power = 0.0F;
+
+        for (ItemStack stack : inventory)
+        {
+            if (!stack.isEmpty())
+            {
+                power += stack.getCount() / (float)Math.min(getInventoryStackLimit(), stack.getMaxStackSize());
+                ++count;
+            }
+        }
+
+        power = power / inventory.size();
+        return MathHelper.floor(power * 14.0F) + (count > 0 ? 1 : 0);
+    }
+
+    @Override
+    public boolean isUsableByPlayer(EntityPlayer player) {
+        return true;
+    }
+
+    @Override
+    public void openInventory(EntityPlayer player) {
+    }
+
+    @Override
+    public void closeInventory(EntityPlayer player) {
+    }
+
+    @Override
+    public int getField(int id) {
+        return 0;
+    }
+
+    @Override
+    public void setField(int id, int value) {
+    }
+
+    @Override
+    public int getFieldCount() {
+        return 0;
+    }
+
+    @Override
+    public void clear() {
+        inventory.clear();
+    }
+
+    @Override
+    public NonNullList<ItemStack> getInventory() {
+        return inventory;
+    }
+
+    @Override
+    public void updateGraphics(int slot) {
+        markDirty();
+    }
+
+    @Override
+    public boolean isItemValidForSlot(int index, ItemStack stack) {
+        return false;
+    }
+}
